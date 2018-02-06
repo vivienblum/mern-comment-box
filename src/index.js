b370e92ebbfeb15af6793b64451c7d955f4a1b17
Anuswan91 <vivien.blum@free.fr>
2018-02-06 07:24:43 -0800

    Posting comments works

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentBox from './CommentBox';
+import React from "react"
+import ReactDOM from "react-dom"
+import CommentBox from "./CommentBox"
 
 ReactDOM.render(
-  <CommentBox />, 
-  document.getElementById('root')
-);
+  <CommentBox url="http://localhost:3001/api/comments" pollInterval={2000} />,
+  document.getElementById("root")
+)
